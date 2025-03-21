b6c0fc574e2fe3a6751dd5eaa89df7f5d3c92a5f
Dan Sealy <dan.sealy5@gmail.com>
2025-03-21 13:20:33 -0400

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import mockTransactions from './components/Data'
+import DashboardLayoutBasic from './components/Primarydash'
+
+// import ScrollRestoration from './components/Transaction'
 
 function App() {
 
   return (
     <>
+      <DashboardLayoutBasic />
       {console.log(mockTransactions)}
     </>
   )
