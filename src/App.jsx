5c0959b362804ab28c3d32821ae6b66d2e3a82a7
Dan Sealy <dan.sealy5@gmail.com>
2025-03-21 12:22:27 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import mockTransactions from './components/Data'
+
+function App() {
+
+  return (
+    <>
+      {console.log(mockTransactions)}
+    </>
+  )
+}
+
+export default App
