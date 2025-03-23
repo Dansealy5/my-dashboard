71721f5a0bdb16836e2dd5857368ee28ba836adf
Dan Sealy <dan.sealy5@gmail.com>
2025-03-23 17:03:36 -0400

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/' element={<Dashboard />} />
     </Routes>
     </>
   )
